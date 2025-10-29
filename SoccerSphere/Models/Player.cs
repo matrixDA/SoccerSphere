81db5e62a2e97f1f043b6bc38fca95b44fdc8c39
Dinesh <dadhikari03@protonmail.com>
2025-10-29 14:25:09 -0400

    Player class with data

--- SoccerSphere/Models/Player.cs
+++ SoccerSphere/Models/Player.cs
@@ -1,6 +1,19 @@
-﻿namespace SoccerSphere.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoccerSphere.Models
 {
     public class Player
     {
+        public int PlayerId { get; set; }
+        [Required(ErrorMessage = "Please enter player's name.")]
+        public string PlayerName { get; set; }
+
+        public int Goals { get; set; }
+
+        public int Assists { get; set; }
+
+        public int MatchesPlayed { get; set; }
+
+        public double Rating { get; set; }
     }
 }
