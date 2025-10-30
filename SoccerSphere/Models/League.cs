8162b7a79a2ea26c284484b068e613dbb3a4d15d
Loc Nguyen <loc.nguyen.leo@gmail.com>
2025-10-30 11:48:40 -0400

    Add League model

---
+++ SoccerSphere/Models/League.cs
@@ -0,0 +1,10 @@
+﻿namespace SoccerSphere.Models
+{
+    public class League
+    {
+        public int leagueId {  get; set; }
+        public string leagueName { get; set; }
+        public string leagueCountry { get; set; }
+        public string leagueContinent { get; set; }
+    }
+}
