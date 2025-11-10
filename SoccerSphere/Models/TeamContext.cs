64fd433a3fe677550b1cbfc2c6fc87fd2ccc7d73
Loc Nguyen <loc.nguyen.leo@gmail.com>
2025-11-10 11:09:05 -0500

    create SoccerSphereDB with starter data

---
+++ SoccerSphere/Models/TeamContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SoccerSphere.Models
+{
+    public class TeamContext : DbContext
+    {
+        public TeamContext(DbContextOptions<TeamContext> options)
+            : base(options) { }
+        public DbSet<Team> teams { get; set; }
+        public DbSet<Player> players { get; set; }
+        public DbSet<League> leagues { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team {TeamId=1, TeamName = "Barcelona", Revenue = 100000000, Country = "Spain", Wins = 9, Loss = 2, Draws = 1 },
+                new Team { TeamId = 2, TeamName = "Barcelona", Revenue = 100000000, Country = "Spain", Wins = 9, Loss = 2, Draws = 1 },
+                new Team { TeamId = 3, TeamName = "Barcelona", Revenue = 100000000, Country = "Spain", Wins = 9, Loss = 2, Draws = 1 },
+                new Team { TeamId = 4, TeamName = "Barcelona", Revenue = 100000000, Country = "Spain", Wins = 9, Loss = 2, Draws = 1 }
+                );
+
+        }
+    }
+}
