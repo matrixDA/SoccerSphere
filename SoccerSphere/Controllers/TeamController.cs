3eabe06f6ab74e91ceeaff0d83c90897ef787999
Loc Nguyen <loc.nguyen.leo@gmail.com>
2025-11-10 10:03:58 -0500

    add teamcontext for data seeding, add validation controller

--- SoccerSphere/Controllers/TeamController.cs
+++ SoccerSphere/Controllers/TeamController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SoccerSphere.Models;
 
 namespace SoccerSphere.Controllers
 {
     public class TeamController : Controller
     {
+        private 
         public IActionResult Index()
         {
+
             return View();
         }
     }
---
+++ SoccerSphere/Controllers/ValidationController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoccerSphere.Controllers
+{
+    public class ValidationController : Controller
+    {
+
+    }
+}
